c0918ee054213247c7d707b5609fceed8c6f03c2
mintball9543 <minseokcomputer@gmail.com>
2024-06-04 01:49:24 +0900

    setting

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.tripshopkorea"
+    compileSdk = 34
+
+    viewBinding{
+        enable = true
+    }
+
+    defaultConfig {
+        applicationId = "com.example.tripshopkorea"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation("org.jsoup:jsoup:1.14.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.journeyapps:zxing-android-embedded:4.1.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+}
